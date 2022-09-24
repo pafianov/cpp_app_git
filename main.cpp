896b00ca5d92e7d67e3a2133e3f2c508b445b26f
s.pafianov <s.pafianov@SERVICE>
2022-09-24 19:52:17 +0300

    Done task #1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+//-----------------------------------------------------------------------------------------------#
+//Task 1. Create an application that displays information about the book in the following format:|
+//-----------------------------------------------------------------------------------------------#
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+ cout << "\tName \t\t: \"The warand the peace\"" << endl;
+ cout << "\tAvtor \t\t:  L.N. Tolstoj" << endl;
+ cout << "\tIzdatelstvo \t:  Piter" << endl;
+ cout << "\tPages \t\t:  500" << endl;
+
 	return 0;
 }
