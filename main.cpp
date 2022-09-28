e6d57525f4928ff3cace9827d35efd5ec9b3d004
s.pafianov <s.pafianov@SERVICE>
2022-09-28 10:33:55 +0300

    Done task #2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
+//------------------------------------------------------------------#
+//Task 2. Create an application that outputs to the console phrase: |
+//------------------------------------------------------------------#
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	cout << "\t\"Kraj lukomor’a dub zelenyj,\a\a\a" << endl;
+	cout << "\tI zolotyj lancyuh na nim:\a\a\a" << endl;
+	cout << "\tShodnya, shonochi kit uchenyj\a\a\a" << endl;
+	cout << "\tNa lancyuhu kruzhl’aje tim\"\a\a\a" << endl;
 	return 0;
 }
